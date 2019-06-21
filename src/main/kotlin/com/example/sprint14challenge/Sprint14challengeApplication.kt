e1709b16d476218f9a28601d2b19e581600d1435
VivekV95 <vivekv210@gmail.com>
2019-06-21 10:03:38 -0500

    Initial Commit

---
+++ src/main/kotlin/com/example/sprint14challenge/Sprint14challengeApplication.kt
@@ -0,0 +1,11 @@
+package com.example.sprint14challenge
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class Sprint14challengeApplication
+
+fun main(args: Array<String>) {
+    runApplication<Sprint14challengeApplication>(*args)
+}
